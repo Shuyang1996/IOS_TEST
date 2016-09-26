5c79bd12e8122b1b1cab804b50c6af8e343f0d5f
Shuyang Yu <yu2@cooper.edu>
2016-09-26 00:18:36 -0400

    initial push

---
+++ Test_only/Test_only/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test_only
+//
+//  Created by 于舒洋 on 9/26/16.
+//  Copyright © 2016 WB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
